190ea139eab3df1ef4e1cee1aaf1b151e5068e05
Joe Ganobsik <jganobsik@gmail.com>
2019-01-02 14:51:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
  
   kickstarter = Nokogiri::HTML(html)
   
+  projects = {
+  
   kickstarter.css("li.project.grid_4").first
   # projects: kickstarter.css("li.project.grid_4")
   project.css("h2.bbcard_name strong a").text
