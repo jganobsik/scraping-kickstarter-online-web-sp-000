433559499437cef14acbacec4ecc2f6cca343c34
Joe Ganobsik <jganobsik@gmail.com>
2019-01-02 14:51:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,6 +20,9 @@ def create_project_hash
   # description: project.css("p.bbcard_blurb").text
   project.css("ul.project-meta span.location-name").text
   
-  
+   kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
   
 end
