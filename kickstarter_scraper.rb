4834284bea7dcffabc5469cf9260b66803bb3382
Joe Ganobsik <jganobsik@gmail.com>
2019-01-02 14:53:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,15 +10,15 @@ def create_project_hash
   
   projects = {
   
-  kickstarter.css("li.project.grid_4").first
+  #kickstarter.css("li.project.grid_4").first
   # projects: kickstarter.css("li.project.grid_4")
-  project.css("h2.bbcard_name strong a").text
+  #project.css("h2.bbcard_name strong a").text
   # title: project.css("h2.bbcard_name strong a").text
-  project.css("div.project-thumbnail a img").attribute("src").value
+  #project.css("div.project-thumbnail a img").attribute("src").value
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  project.css("p.bbcard_blurb").text
+  #project.css("p.bbcard_blurb").text
   # description: project.css("p.bbcard_blurb").text
-  project.css("ul.project-meta span.location-name").text
+  #project.css("ul.project-meta span.location-name").text
   
    kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
