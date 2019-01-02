d0d15bc22dfb613b668c3c20c57de57b3f058a93
Joe Ganobsik <jganobsik@gmail.com>
2019-01-02 14:47:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,6 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   project.css("div.project-thumbnail a img").attribute("src").value
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  project.css("p.bbcard_blurb").text
+  
 end
